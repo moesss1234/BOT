00cb0319e0508ade6d2288a0e999a10dabdeab59
moesss1234 <62344688+moesss1234@users.noreply.github.com>
2020-06-07 04:29:15 +0700

    Add files via upload

--- bot-checktoken.php
+++ bot-checktoken.php
@@ -4,13 +4,13 @@
 $API_URL = 'https://api.line.me/v2/bot/message';
 $ACCESS_TOKEN = '3r2ohGof69ms4hYeSENpaK8E8fBgGV42UkS/a/gzGc88hTHOpNw5+1E3QkAD4E+ENudqYyIIepXAaPZu1pzPcA82PVd0nSyQGA/TQZcAF4BIZt6i8Nhnqp0Uvc9IzqSyg07kI82CK5yUTktOrq6f2AdB04t89/1O/w1cDnyilFU='; 
 $channelSecret = '7bde955df2a0b989334fd1dcab6c8aee';
-
-
+$url_content='https://api.line.me/v2/bot/message/”.$msg_id.”/content';
+$url1 = "https://api.line.me/v2/bot/profile/".$userId;
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
 
 $request = file_get_contents('php://input');   // Get request content
 $request_array = json_decode($request, true);   // Decode JSON to Array
-
+$userId = $request_array['events'][0]['source']['userId'];
 
 
 if ( sizeof($request_array['events']) > 0 ) {
@@ -23,7 +23,7 @@ if ( sizeof($request_array['events']) > 0 ) {
 
         $data = [
             'replyToken' => $reply_token,
-            'messages' => [['type' => 'text', 'text' => json_encode($request_array)]]
+            'messages' => [['type' => 'text', 'text' => json_encode(getLINEProfile($url1))]]
         ];
         $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
 
@@ -52,5 +52,16 @@ function send_reply_message($url, $post_header, $post_body)
 
     return $result;
 }
+function getLINEProfile($datas)
+	{
+		$ch = curl_init($url1);		
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        $result = curl_exec($ch);
+        curl_close($ch);
 
+    return $result;
+}
 ?>
